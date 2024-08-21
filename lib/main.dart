9c86f79aee62375edc07336771a0b07cd1c7e33c
Ashutosh Kandpal <ashutoshkanpal240@gmail.com>
2024-08-21 11:37:08 +0530

    Fluuter Day 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/home_page.dart';
+import 'package:my_app/Pages/home_page.dart';
+import 'package:my_app/Pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      
+      routes: {
+        "/":(context) => HomePage(), 
+        "/login":(context) => LoginPage(),
+      },
     );
   }
 }
