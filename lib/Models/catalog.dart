fb60ff79f7730f13167ca9055452c91084031f86
Ashutosh Kandpal <ashutoshkanpal240@gmail.com>
2024-08-23 14:05:05 +0530

    Product added

--- lib/Models/catalog.dart
+++ lib/Models/catalog.dart
@@ -8,3 +8,6 @@ class Item{
 
   Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
 }
+
+final Product=[
+Item(id: "500012", name: "Apple I Phone", desc: "I Phone 15 pro", price: 120000, color: "Blue", image: "assets/iphone15_jpg.png")];
