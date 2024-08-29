119620478dae8f9de0e65936b580200f4b56888a
Ashutosh Kandpal <ashutoshkanpal240@gmail.com>
2024-08-29 11:56:33 +0530

    Day 10 Completed

--- lib/Models/catalog.dart
+++ lib/Models/catalog.dart
@@ -1,4 +1,4 @@
-class Item{
+class Item {
   final String id;
   final String name;
   final String desc;
@@ -6,8 +6,21 @@ class Item{
   final String color;
   final String image;
 
-  Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
+  Item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.image});
 }
 
-final Product=[
-Item(id: "500012", name: "Apple I Phone", desc: "I Phone 15 pro", price: 120000, color: "Blue", image: "assets/iphone15_jpg.png")];
+final Product = [
+  Item(
+      id: "500012",
+      name: "Apple I Phone",
+      desc: "I Phone 15 pro",
+      price: 120000,
+      color: "Blue",
+      image: "assets/iphone15_jpg.png")
+];
